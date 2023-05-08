c53ffde6f6b8e1e0d1b802bed715648c125f7b49
Adi Navale <adityasnavale@gmail.com>
2023-05-08 01:21:53 -0700

    Made structs signed

--- project_files/fir_structs.sv
+++ project_files/fir_structs.sv
@@ -1,21 +1,21 @@
 typedef struct packed {
-    logic [23:0] I;
-    logic [23:0] Q;
+    logic signed [23:0] I;
+    logic signed [23:0] Q;
 } Samp;
 
 typedef struct packed {
-    logic [26:0] I;
-    logic [26:0] Q;
+    logic signed [26:0] I;
+    logic signed [26:0] Q;
 } Coef;
 
 //1.23 format
 typedef struct packed{
-    logic [23:0] I; //real
-    logic [23:0] Q; //imaginary
+    logic signed [23:0] I; //real
+    logic signed [23:0] Q; //imaginary
 } Sum;
 
 //4.47 format
 typedef struct packed{
-    logic [50:0] I;
-    logic [50:0] Q;
+    logic signed [50:0] I;
+    logic signed [50:0] Q;
 } Partial_product;
