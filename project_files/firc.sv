d5337b0544a06c97d69426e8e61292e86de4b19d
Adi Navale <adityasnavale@gmail.com>
2023-05-08 00:36:20 -0700

    Samples are now shifted in and out

--- project_files/firc.sv
+++ project_files/firc.sv
@@ -6,12 +6,12 @@ module firc(
     input logic Reset,
     input logic PushIn,
     output logic StopIn,
-    input logic [23:0] SampI,
-    input logic [23:0] SampQ,
+    input logic signed [23:0] SampI,
+    input logic signed [23:0] SampQ,
     input logic PushCoef,
     input logic [4:0] CoefAddr,
-    input logic [26:0] CoefI,
-    input logic [26:0] CoefQ,
+    input logic signed [26:0] CoefI,
+    input logic signed [26:0] CoefQ,
     output logic PushOut,
     output logic [31:0] FI,
     output logic [31:0] FQ
@@ -76,6 +76,32 @@ module firc(
             coefQ[CoefAddr] = CoefQ;
             $display("Time: %dns \t CoefAddr: %d \t coefI: %x", $realtime, CoefAddr, coefI[CoefAddr]);
             $display("Time: %dns \t CoefAddr: %d \t coefQ: %x \n", $realtime, CoefAddr, coefQ[CoefAddr]);
+        end
+    end
+
+    //Handling of samples that are being input to the adders before they go through complex multiplies.
+    Samp cur_samp[29]; //1.23 format
+
+    always @ (posedge Clk or posedge Reset) begin
+        if(cur_state == idle && Reset) begin
+            for(int i = 0; i < 29; i = i + 1) begin
+                cur_samp[i] <= 0;
+            end
+        end else if(cur_state == mac && PushIn) begin
+            //Shift out the oldest sample
+            for(int i = 0; i < 28; i = i + 1) begin
+                cur_samp[i + 1].I <= cur_samp[i].I;
+                cur_samp[i + 1].Q <= cur_samp[i].Q;
+            end
+
+            //Shift in the newest sample
+            cur_samp[0].I <= SampI;
+            cur_samp[0].Q <= SampQ;
+
+            //Display for debug
+            for(int i = 0; i < 29; i = i + 1) begin
+                $display("Time: %d ns \t cursamp[%d].I = %d \t cursamp[%d].Q = %d", $realtime, i, cur_samp[i].I, i, cur_samp[i].Q);
+            end
         end
     end
 endmodule : firc
